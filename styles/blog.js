59ece50fd7f601bf30d0d04e4086efd297511944
sourav-dn <souravdn.dev@gmail.com>
2025-03-02 17:20:53 +0600

    first commit

---
+++ styles/blog.js
@@ -0,0 +1,5 @@
+
+// go to main page
+document.getElementById("btn-back-desk").addEventListener("click", function(){
+    window.location.href = "./index.html";
+})
---
+++ styles/style.js
@@ -0,0 +1,60 @@
+// Navbar color button for color changing
+document.getElementById("btn-color-changer").addEventListener("click", function () {
+    const colors = ["#FF5733", "#33FF57", "#3357FF", "#FFD700", "#8A2BE2"];
+    let body = document.body;
+    navbar.classList.remove("bg-gray-100");
+    let randomColor = Math.floor(Math.random() * colors.length);
+    body.style.backgroundColor = colors[randomColor];
+});
+
+
+// showing current date and time
+const dateTimeDay = new Date();
+document.getElementById("date-time").innerHTML = dateTimeDay.toDateString();
+
+
+
+// completed buttons 
+const buttons = document.querySelectorAll(".btn-complete");
+
+for (const button of buttons) {
+    button.addEventListener("click", function(event) {
+        const clickedButton = event.target;
+        const taskContainer = clickedButton.closest('.task-container');
+        const taskTitle = taskContainer.querySelector('h2.task-box').textContent;       
+        alert("Board Updated Successfully");
+
+        
+        const taskAssignNumber = document.querySelector("#task-assign-num");
+        const navbarNumber = document.querySelector("#navbar-num");
+        const activityLog = document.querySelector("#activity-log");
+        let currentTasks = parseInt(taskAssignNumber.textContent);
+        let currentNavbarNumber = parseInt(navbarNumber.textContent);
+        
+        if (currentTasks > 0) {  
+            taskAssignNumber.textContent = currentTasks - 1;
+            navbarNumber.textContent = currentNavbarNumber + 1;
+
+            
+            const timestamp = new Date().toLocaleTimeString();
+            const logEntry = `<p>You have Completed The Task ${taskTitle} at ${timestamp}</p>`;
+            const currentEntry = activityLog.innerHTML;
+            activityLog.innerHTML = currentEntry + logEntry;
+            clickedButton.disabled = true;
+        }    
+            
+    });
+    const clearHistoryButton = document.querySelector("#btn-clear-history");
+    if (clearHistoryButton) {
+        clearHistoryButton.addEventListener("click", function() {
+            const activityLog = document.querySelector("#activity-log");
+            activityLog.innerHTML = "";
+        });
+    }
+}
+
+
+// go to blog page
+document.getElementById("date-section").addEventListener("click", function(){
+     window.location.href = "./blog.html";
+})
